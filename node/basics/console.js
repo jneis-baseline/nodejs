cadb312a09f17f93c11a80f359a643d3ba7e492f
Josue Neis <jneis@users.noreply.github.com>
2015-11-01 15:04:23 -0200

    console

---
+++ node/basics/console.js
@@ -0,0 +1,31 @@
+// console, or require('console')
+// - object is a global object
+// - output is sent to process.stdout or process.stderr
+// - functions are sync for terminal, async for pipes
+
+console.log('message', 'without', 'placeholders');
+
+console.log('%s %d %j', 'string', 1, {json: 'obj'}, {js: 'obj'});
+
+console.info('same as console.log');
+
+console.error('prints %s', 'to', 'stderr');
+
+console.warn('same', 'as', 'console.error');
+
+console.time('timer-name');
+for (var i = 0; i < 100000000   ; i++) { ; }
+console.timeEnd('timer-name');
+
+console.trace('prints message', 'plus stack trace');
+
+// in order to customize output:
+var Console = console.Console;
+var fs = require('fs');
+
+var output = fs.createWriteStream('./logs/stdout.log');
+var errorOutput = fs.createWriteStream('./logs/stderr.log');
+
+var logger = new Console(output, errorOutput);
+logger.log('simple logging %s', 'messages');
+logger.error('error', 'logging');
