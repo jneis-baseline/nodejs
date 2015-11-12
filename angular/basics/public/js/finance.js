aa06ba23b54e3732aec4e335b330a133f7f49fdc
Josue Neis <jneis@users.noreply.github.com>
2015-11-12 11:45:25 -0200

    accessing backend

--- angular/basics/public/js/finance.js
+++ angular/basics/public/js/finance.js
@@ -1,9 +1,15 @@
 // creates a new module with no deps
 angular.module('finance', [])
 
-	// creates a new service with no deps
-	.factory('currencyConverter', function() {
+	// creates a new service with a dep from core
+	.factory('currencyConverter', ['$http', function($http) {
 		
+		var YAHOO_URL = '//query.yahooapis.com/v1/public/yql?q=' +
+			'select * from yahoo.finance.xchange where pair in ("PAIRS")' +
+			'&format=json' +
+			'&env=store://datatables.org/alltableswithkeys' +
+			'&callback=JSON_CALLBACK'
+
 		var currencies = ['USD', 'EUR', 'CNY'];
 
 		var rates = {
@@ -16,10 +22,25 @@ angular.module('finance', [])
 			return amount * rates[from] / rates[to];
 		};
 
+		var refresh = function() {
+			var url = YAHOO_URL.replace('PAIRS', 'USD' + currencies.join('","USD'));
+
+			return $http.jsonp(url).success(function(data) {
+				var newRates = [];
+				angular.forEach(data.query.results.rate, function(rate) {
+					var currency = rate.id.substring(3, 6);
+					newRates[currency] = window.parseFloat(rate.Rate);
+				});
+				rates = newRates;
+			});
+		};
+
+		refresh();
+
 		// obj exposed by this service
 		return {
 			currencies: currencies,
 			convert: convert
 		};
 
-	});
+	}]);
---
+++ angular/basics/public/js/finance.js.3
@@ -0,0 +1,25 @@
+// creates a new module with no deps
+angular.module('finance', [])
+
+	// creates a new service with no deps
+	.factory('currencyConverter', function() {
+		
+		var currencies = ['USD', 'EUR', 'CNY'];
+
+		var rates = {
+			USD: 1,
+			EUR: 0.74,
+			CNY: 6.09
+		};
+
+		var convert = function(amount, from, to) {
+			return amount * rates[from] / rates[to];
+		};
+
+		// obj exposed by this service
+		return {
+			currencies: currencies,
+			convert: convert
+		};
+
+	});
